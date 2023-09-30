9da514f308a2f122609b8f8c339a97141dbc5b1b
Pranshu321 <pranshujain0111@gmail.com>
2023-10-01 00:01:08 +0530

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/components/*.{js,jsx}",
+    "node_modules/preline/dist/*.js",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#02897A",
+        text: "#22343D",
+      },
+      backgroundImage: {},
+      boxShadow: {
+        borderShadow: "0px 2px 8px rgba(0, 0, 0, 0.25)",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/aspect-ratio"), require("preline/plugin")],
+};
