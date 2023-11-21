564fef02ad5a7171d3ddbe3ff6df9a85a4482c91
pankhuri-deshmukh <pankhurideshmukh1000@gmail.com>
2023-11-21 20:37:39 +0530

    colour changed

--- src/components/dasboard/pages/AceEvaluator.js
+++ src/components/dasboard/pages/AceEvaluator.js
@@ -95,7 +95,7 @@ const AceEvaluator = () => {
     return (
       <Layout>
     
-        <div className="text-center mt-8">
+        <div className="text-center mt-4">
           <h1 className="text-2xl font-bold mb-4">Academic Content Evaluator</h1>
         </div>
     
@@ -107,7 +107,7 @@ const AceEvaluator = () => {
 
               <div className="stat">
                 <div className="stat-figure text-secondary">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-teal-500"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 </div>
                 <div className="stat-title">Downloads</div>
                 <div className="stat-value">31K</div>
@@ -116,7 +116,7 @@ const AceEvaluator = () => {
               
               <div className="stat ">
                 <div className="stat-figure text-secondary">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-teal-500"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
                 </div>
                 <div className="stat-title">New Users</div>
                 <div className="stat-value">4,200</div>
@@ -125,7 +125,7 @@ const AceEvaluator = () => {
               
               <div className="stat ">
                 <div className="stat-figure text-secondary">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-teal-500"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
                 </div>
                 <div className="stat-title">New Registers</div>
                 <div className="stat-value">1,200</div>
@@ -170,6 +170,7 @@ const AceEvaluator = () => {
               }} />
             </div>
             <h1 className="text-lg mt-4">The total rating of the document is: {score / 10}/10</h1>
+            <p className='mt-3'>Note: Lorem ipsum dolor est lorem ipsum dolor est Lorem ipsum dolor est lorem ipsum dolor est Lorem ipsum dolor est lorem ipsum dolor est</p>
           </div>
         </div>
 
